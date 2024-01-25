018a60475ae86fc99214a39e8502a07698e2bad3
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-01-25 17:45:22 -0600

    Save json to seperate file so dont need to run api each time

---
+++ CreateNOTAM.py
@@ -0,0 +1,18 @@
+import requests
+import credentials
+import json 
+
+# 
+url = 'https://external-api.faa.gov/notamapi/v1/notams'
+headers = {'client_id': credentials.clientID,'client_secret': credentials.clientSecret}
+
+req = requests.get(url, headers=headers)
+
+parsed_req = json.loads(req.text)
+
+print(f"{req.status_code}\n\n")
+print(f"{req.headers}\n\n")
+print(f"{req.text}\n\n")
+
+with open("TestData/TestNOTAM.json", 'w') as json_file:
+    json.dump(parsed_req['items'], json_file)
---
+++ ParseNOTAM.py
@@ -0,0 +1,12 @@
+import json
+
+# File path where the JSON data is saved
+file_path = "TestData/TestNOTAM.json"
+
+# Reading JSON data from the file
+with open(file_path, 'r') as json_file:
+    loaded_data = json.load(json_file)
+
+# Accessing the loaded JSON data
+for item in loaded_data:
+    print(item['type'])
