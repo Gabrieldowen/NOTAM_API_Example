2a91d4e764d25c8ede129744e6422c2b2127aa81
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-01-26 17:11:05 -0600

    add some comments

--- CreateNOTAM.py
+++ CreateNOTAM.py
@@ -2,7 +2,9 @@ import requests
 import credentials
 import json 
 
-# 
+# To add parameters to seach we need to append to the url like example below
+# https://external-api.faa.gov/notamapi/v1/notams?icaoLocation=KLAX&domesticLocation=JFK&notamType=N&classification=INTL&effectiveStartDate=YYYY-MM-DDTHH%3AMM&effectiveEndDate=YYYY-MM-DDTHH%3AMM&featureType=RWY&locationLongitude=65&locationLatitude=30&locationRadius=10&lastUpdatedDate=10&sortBy=domesticLocation&sortOrder=Asc 
+
 url = 'https://external-api.faa.gov/notamapi/v1/notams'
 headers = {'client_id': credentials.clientID,'client_secret': credentials.clientSecret}
 
