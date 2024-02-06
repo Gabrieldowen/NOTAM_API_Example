92db1113097c7a127e98927e251401b7dcd699c8
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-02-06 11:40:23 -0600

    add more print statements

--- ParseNOTAM.py
+++ ParseNOTAM.py
@@ -7,6 +7,18 @@ file_path = "static/TestData/TestNOTAM.json"
 with open(file_path, 'r') as json_file:
     loaded_data = json.load(json_file)
 
+# print key names of each NOTAM
+print(loaded_data['items'][0].keys())
+
+# print components on the first NOTAM
+print(f"\n\n type: {loaded_data['items'][0]['type']}")
+print(f"\n\n properties: {loaded_data['items'][0]['properties']}")
+print(f"\n\n geometry: {loaded_data['items'][0]['geometry']}")
+
+# things we will need
+print(f"\n\n notam: {loaded_data['items'][0]['properties']['coreNOTAMData']['notam']}")
+print(f"\n\n text: {loaded_data['items'][0]['properties']['coreNOTAMData']['notam']['text']}")
+
 # Accessing the loaded JSON data
-for item in loaded_data['items']:
-    print(item['type'])
+#for item in loaded_data['items']:
+    #print(item['type'])
