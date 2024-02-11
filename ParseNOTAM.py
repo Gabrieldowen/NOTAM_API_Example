33c01ef9443b1a5ce11be04e069d752b5d8a16fa
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-02-10 20:00:14 -0600

    change notam model and parseNOTAM
    change each item in class to be optional. Then changed the parse script to take each NOTAM from the Json and create a class then adding that to a list

--- ParseNOTAM.py
+++ ParseNOTAM.py
@@ -1,24 +1,19 @@
 import json
+import models
 
-# File path where the JSON data is saved
-file_path = "static/TestData/TestNOTAM.json"
+def ParseNOTAM():
+    # File path where the JSON data is saved
+    file_path = "static/TestData/TestNOTAM.json"
 
-# Reading JSON data from the file
-with open(file_path, 'r') as json_file:
-    loaded_data = json.load(json_file)
+    # Reading JSON data from the file
+    with open(file_path, 'r') as json_file:
+        loaded_data = json.load(json_file)
 
-# print key names of each NOTAM
-print(loaded_data['items'][0].keys())
+    # create a class for each NOTAM
+    NOTAMs = []
+    for item in loaded_data['items']:
+        NOTAMs.append(models.Notam(item['properties']['coreNOTAMData']['notam']))
+        
+    return NOTAMs
 
-# print components on the first NOTAM
-print(f"\n\n type: {loaded_data['items'][0]['type']}")
-print(f"\n\n properties: {loaded_data['items'][0]['properties']}")
-print(f"\n\n geometry: {loaded_data['items'][0]['geometry']}")
-
-# things we will need
-print(f"\n\n notam: {loaded_data['items'][0]['properties']['coreNOTAMData']['notam']}")
-print(f"\n\n text: {loaded_data['items'][0]['properties']['coreNOTAMData']['notam']['text']}")
-
-# Accessing the loaded JSON data
-#for item in loaded_data['items']:
-    #print(item['type'])
+ParseNOTAM()
