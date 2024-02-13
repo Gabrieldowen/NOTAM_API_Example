df3277f7d1c3021fe413214640c46924c60e9ad7
Blue_water_okie <54322474+okie-creator@users.noreply.github.com>
2024-02-13 13:25:45 -0600

    Add files via upload
    Updated ZuluConverter

--- ZuluConverter.py
+++ ZuluConverter.py
@@ -27,9 +27,10 @@ def convert_est_to_zulu(est_datetime):
     # Define time zones
     est_tz = timezone('America/New_York')    # Eastern Standard Time
     zulu_tz = timezone('UTC')   # Universal Time (Zulu)
-
+    date_format = "%Y-%m-%d %H:%M:%S"
+    est = datetime.strptime(est_datetime, date_format)
     # Localize the input EST datetime
-    localized_est = est_tz.localize(est_datetime)
+    localized_est = est_tz.localize(est)
 
     # Convert the localized EST datetime to Zulu time
     zulu_datetime = localized_est.astimezone(zulu_tz)
@@ -38,14 +39,14 @@ def convert_est_to_zulu(est_datetime):
 
     return zulu_datetime_formatted
 
-print(convert_est_to_zulu(datetime.today()))
 
 #PST to Zulu
 def convert_pst_to_zulu(pst_datetime):
     pst_tz = timezone('America/Los_Angeles')    # Pacific Standard Time
     zulu_tz = timezone('UTC')   # Universal Time (Zulu)
-
-    localized_pst = pst_tz.localize(pst_datetime)
+    date_format = "%Y-%m-%d %H:%M:%S"
+    pst = datetime.strptime(pst_datetime, date_format)
+    localized_pst = pst_tz.localize(pst)
 
     zulu_datetime = localized_pst.astimezone(zulu_tz)
 
@@ -53,14 +54,15 @@ def convert_pst_to_zulu(pst_datetime):
 
     return zulu_datetime_formatted
 
-print(convert_pst_to_zulu(datetime.today()))
+
 
 #MST to Zulu
 def convert_mst_to_zulu(mst_datetime):
     mst_tz = timezone('America/Denver')     # Mountain Standard Time
     zulu_tz = timezone('UTC')   # Universal Time (Zulu)
-
-    localized_mst = mst_tz.localize(mst_datetime)
+    date_format = "%Y-%m-%d %H:%M:%S"
+    mst = datetime.strptime(mst_datetime, date_format)
+    localized_mst = mst_tz.localize(mst)
 
     zulu_datetime = localized_mst.astimezone(zulu_tz)
 
@@ -68,14 +70,14 @@ def convert_mst_to_zulu(mst_datetime):
 
     return zulu_datetime_formatted
 
-print(convert_mst_to_zulu(datetime.today()))
 
 #HST to Zulu
 def convert_hst_to_zulu(hst_datatime):
     hst_tz = timezone('US/Hawaii')     # Hawaii Standard Time
     zulu_tz = timezone('UTC')   # Universal Time (Zulu)
-
-    localized_hst = hst_tz.localize(hst_datatime)
+    date_format = "%Y-%m-%d %H:%M:%S"
+    hst = datetime.strptime(hst_datatime, date_format)
+    localized_hst = hst_tz.localize(hst)
 
     zulu_datetime = localized_hst.astimezone(zulu_tz)
 
@@ -83,19 +85,18 @@ def convert_hst_to_zulu(hst_datatime):
 
     return zulu_datetime_formatted
 
-print(convert_hst_to_zulu(datetime.today()))
+
 
 #AKST to Zulu
 def convert_akst_to_zulu(akst_datatime):
     akst_tz = timezone('US/Alaska')     # Alaska Standard Time
     zulu_tz = timezone('UTC')   # Universal Time (Zulu)
-
-    localized_akst = akst_tz.localize(akst_datatime)
+    date_format = "%Y-%m-%d %H:%M:%S"
+    akst = datetime.strptime(akst_datatime, date_format)
+    localized_akst = akst_tz.localize(akst)
 
     zulu_datetime = localized_akst.astimezone(zulu_tz)
 
     zulu_datetime_formatted = zulu_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
     return zulu_datetime_formatted
-
-print(convert_akst_to_zulu(datetime.today()))
