96e5f15bbdcec4b0bdf68a14851686204e45e562
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-02-21 19:14:37 -0600

    add unit test for calculating bearing

--- MinimalCirclesPathTest.py
+++ MinimalCirclesPathTest.py
@@ -1,8 +1,8 @@
 from MinimalCirclesPath import *
 import unittest
 
-class TestGetDistance(unittest.TestCase):
-	def testUS(self):
+class TestMinimalCirclesPath(unittest.TestCase):
+	def testGetDistance(self):
 		# tests gets distance betweeen two points (haversine)
 		# test data from https://www.nhc.noaa.gov/gccalc.shtml
 
@@ -15,6 +15,13 @@ class TestGetDistance(unittest.TestCase):
 		# RSW to LAX 
 		self.assertAlmostEqual(getDistance(26.542, -81.755, 33.943, -118.407), 1940, places = -1)
 
-	def testForeign(self):
 		# japan to germany (CTS to ZVM)
 		self.assertAlmostEqual(getDistance(42.8, 141.667, 49.467, -96.833), 4470, places = -1)
+
+
+	def testCalculateBearing(self):
+		# tests method that gets bearing given two points
+		# test data from https://www.movable-type.co.uk/scripts/latlong.html
+
+		# DFW to DEN
+		self.assertAlmostEqual(calculateBearing(32.897, -97.038, 39.862, -104.673), 320.78, places = 2)
