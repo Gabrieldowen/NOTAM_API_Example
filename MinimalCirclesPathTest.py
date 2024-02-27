5cd4ee8eee14bede2c8bef7d856e6b8df7f775bf
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-02-27 15:46:29 -0600

    fix formatting and add comments

--- MinimalCirclesPathTest.py
+++ MinimalCirclesPathTest.py
@@ -2,10 +2,11 @@ from MinimalCirclesPath import *
 import unittest
 
 class TestMinimalCirclesPath(unittest.TestCase):
-	def testGetDistance(self):
-		# tests gets distance betweeen two points (haversine)
-		# test data from https://www.nhc.noaa.gov/gccalc.shtml
 
+	# tests gets distance betweeen two points (haversine)
+	# test data from https://www.nhc.noaa.gov/gccalc.shtml
+	def testGetDistance(self):
+		
 		# DFW to DEN
 		self.assertAlmostEqual(getDistance(32.897, -97.038, 39.862, -104.673), 557, places = 0)
 
@@ -22,30 +23,36 @@ class TestMinimalCirclesPath(unittest.TestCase):
 		self.assertAlmostEqual(getDistance(42.8, 141.667, 49.467, -96.833), 4470, places = -1)
 
 
+	# tests method that gets bearing given two points
+	# test data from https://www.movable-type.co.uk/scripts/latlong.html
 	def testCalculateBearing(self):
-		# tests method that gets bearing given two points
-		# test data from https://www.movable-type.co.uk/scripts/latlong.html
- 
+		
 		# DFW to DEN
 		self.assertAlmostEqual(calculateBearing(32.897, -97.038, 39.862, -104.673), 320.78, places = 2)
 
-	def testGetPath(self):
-		# tests that the amount of points returned is less than the number of circles lined edge to edge
+
+	# tests that the final NOTAM covers the furthers point we want covered
+	def testGetPathFinalPoint(self):
 
 		# DFW to DEN
 		radius = 100
 		destLat=39.862
 		destLong=-104.673
 		pathWidth=50
-		updateDest = nextPoint(destLat, destLong, 320.7808955450159, (pathWidth/2))
+		bearing = 320.7808955450159
 
+		# gets the final point we want covered
+		updateDest = nextPoint(destLat, destLong, bearing, (pathWidth/2))
+
+		# gets all the points for NOTAMs we would cover from DFW to DEN
 		DFW_DEN = getPath(startLat=32.897, startLong=-97.038, destLat=destLat, destLong=destLong, radius=radius, pathWidth=pathWidth)
-		message = f"\nthe distance between ({DFW_DEN[-1][0]}, {DFW_DEN[-1][1]}) and ({updateDest[0]}, {updateDest[1]}) needs to be less than {radius-(pathWidth/2)}"
 
-		# this is making sure the distance from the last search point encapsulates the final area we want covered
-		# the radius of the NOTAM circle is greater than the distance from the last point to the dest plus the desired range
-		self.assertGreater(radius, getDistance(DFW_DEN[-1][0], DFW_DEN[-1][1], updateDest[0], updateDest[1]), message)
+		# checks if the final NOTAM includes the last point we want covered
+		self.assertGreater(radius, getDistance(DFW_DEN[-1][0], DFW_DEN[-1][1], updateDest[0], updateDest[1]))
 
+
+	# tests that the amount of points returned is less than the number of circles lined edge to edge
+	def testGetPathNumPoints(self):
 
 		# HAX to MKO (two airports close in Muskogee Oklahoma)
 		self.assertAlmostEqual(len(getPath(35.746, -95.413, 35.667, -95.367, 100, 50)), 1, places = 0)
