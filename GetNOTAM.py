e963a9eb83d4eb2dbf5acd6cb2a4f46b68c2a2b0
BrendenDeWitt <70327934+BrendenDeWitt@users.noreply.github.com>
2024-03-08 17:54:36 -0600

    Updated GetNOTAM.py
    Fixed the indents.

--- GetNOTAM.py
+++ GetNOTAM.py
@@ -18,19 +18,19 @@ def getNotam(effectiveStartDate, effectiveEndDate, longitude, latitude, pageNum,
   effectiveEndDate = datetime.strptime(effectiveEndDate, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%dT%H:%M:%SZ")  
   
   url = 'https://external-api.faa.gov/notamapi/v1/notams'
-    url = (f"{url}?responseFormat=geoJson&effectiveStartDate={effectiveStartDate}"
+  url = (f"{url}?responseFormat=geoJson&effectiveStartDate={effectiveStartDate}"
        f"&effectiveEndDate={effectiveEndDate}&locationLongitude={longitude}"
        f"&locationLatitude={latitude}&locationRadius={radius}"
        f"&pageNum={pageNum}&pageSize=50"
        f"&sortBy=notamType&sortOrder=Asc")
 
-    headers = {'client_id': credentials.clientID, 'client_secret': credentials.clientSecret}
+  headers = {'client_id': credentials.clientID, 'client_secret': credentials.clientSecret}
 
-    req = requests.get(url, headers=headers)
+  req = requests.get(url, headers=headers)
     
-    parsed_req = req.json()
+  parsed_req = req.json()
     
-    return parsed_req
+  return parsed_req
 
 #buildNotam: does multiple API calls of a location given in inputs and combines all Jsons of each page into a single Json file
 #@returns: combinded_core_notam_data, the combinded Json of all pages for one location
