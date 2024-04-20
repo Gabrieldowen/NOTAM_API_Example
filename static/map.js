cf3a3d5edea86e3f9afaf715775b6d8a236b4ccd
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-04-19 19:52:39 -0500

    modify mapping

--- static/map.js
+++ static/map.js
@@ -14,17 +14,13 @@ $(document).ready( function() {
 });
 
 function loadMap() {
-
-    if (calledPoints.type === "FeatureCollection") {
-        // alert("The GeoJSON data has the correct type: FeatureCollection");
-    } else {
-       alert("The GeoJSON data does not have the correct type: FeatureCollection");
-    }
     
+    // map settings
     var coordinatesLength = calledPoints.features.length;
     middlePoint = parseInt(coordinatesLength/2)
     var zoom  = 4
-    // alert("coordinates" + calledPoints.features[0].geometry.coordinates)
+    
+    // centers the map at the middle point of the called points
     var map = L.map('map').setView([ calledPoints.features[middlePoint].geometry.coordinates[1],  calledPoints.features[middlePoint].geometry.coordinates[0]], zoom );
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -32,21 +28,40 @@ function loadMap() {
     }).addTo(map);
 
 
-    // map the points that are called
+    // map the points that are called to get NOTAMs
     var myLayer = L.geoJSON().addTo(map);
+    var line = [];
+
+    // for each of the called points map a point and save it to a array for a line
     calledPoints.features.forEach(function(feature) {
+
         myLayer.addData(feature);
+        var point = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]]
+
+        // Create a circle marker for each point and add it to the map
+        L.circle(point, {
+                color: 'blue',
+                fillColor: '#0000FF',
+                fillOpacity: 0.25,
+                radius: 100 * 1852 // nautical miles to meters
+            }).addTo(map)
+
+        // save each point to a array for the line
+        line.push(point);
+        
     });
 
-    // point = notamCoords[0].toString().split(",")
-    // alert(point[1] +", " + point[0])
-    // var circle = L.circle([parseFloat(point[0]), parseFloat(point[1])], {
-    //     color: 'red',
-    //     fillColor: '#f03',
-    //     fillOpacity: 0.5,
-    //     radius: 500
-    // }).addTo(map);
 
+    // create a line from the points
+    var polyline = new L.polyline(line, {
+        color: 'blue',
+        weight: 3,
+        opacity: 0,
+        smoothFactor: 1
+    });
+    polyline.addTo(map);
+    
+    // map the notams as red dots
     notamCoords.forEach(function(coord) {
         // Split the coordinate string
         var point = coord.toString().split(",");
@@ -64,6 +79,7 @@ function loadMap() {
 
 }
 
+// this function parses the existing coordinates to a format that can be used by leaflet
 function parseCoord(coordString) {
     // Extract degrees and minutes for latitude
     var latDegrees = parseInt(coordString.substring(0, 2), 10);
